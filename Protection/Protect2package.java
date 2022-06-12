caf2c1de3f62e86883ac2750f1b68688daa549a7
ashitkumar7 <101192971+ashitkumar7@users.noreply.github.com>
2022-06-12 16:32:56 +0530

    Create Protect2package.java

---
+++ Protection/Protect2package.java
@@ -0,0 +1,17 @@
+package Protection;
+
+
+
+public class Protect2package extends Protect1package {
+
+    public void readStringextends() {
+
+        System.out.println("Public String accessible within package : " + str1);
+
+        System.out.println("Private String is not accessible within package");
+
+        System.out.println("Protected String accessible within package : " + str3);
+
+    }
+
+}
